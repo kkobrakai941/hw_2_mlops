40678e2da62e9e3b2e46d1095906c9bdd1bb93c5
kkobrakai941 <Amir2016kzn@gmail.com>
2025-11-02 13:27:04 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,13 @@
+import os
+from src.preprocessing import preprocess
+from src.scorer import score
+
+INPUT_PATH = "./input/test.csv"
+REFERENCE_PATH = "./train_data/train.csv"
+MODEL_PATH = "./models/my_catboost.cbm"
+OUTPUT_DIR = "./output"
+
+if __name__ == "__main__":
+    X_test = preprocess(INPUT_PATH, REFERENCE_PATH)
+    score(X_test, MODEL_PATH, OUTPUT_DIR)
 
