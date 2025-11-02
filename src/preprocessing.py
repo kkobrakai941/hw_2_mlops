1f5dc3ec6c135b6fea6c61ef34dc9e41edf5e401
kkobrakai941 <Amir2016kzn@gmail.com>
2025-11-02 14:28:18 +0300

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,19 @@
+import pandas as pd
+
+def preprocess(input_path: str, reference_path: str = "train_data/train.csv") -> pd.DataFrame:
+    
+    df_test = pd.read_csv(input_path)
+    df_train = pd.read_csv(reference_path)
+
+    if "target" in df_train.columns:
+        df_train = df_train.drop(columns=["target"])
+
+    common_cols = [c for c in df_train.columns if c in df_test.columns]
+    df_test = df_test[common_cols]
+    for c in df_test.columns:
+        if df_test[c].dtype == "object":
+            df_test[c] = df_test[c].astype(str)
+    df_test = df_test.fillna(0)
+
+    return df_test
 
